606b2afc47aa4877a8370fbfe171a0e4a005f76a
Alyona <kudinova0208@gmail.com>
2024-12-27 14:51:35 +0300

    'Log'

---
+++ source.cpp
@@ -0,0 +1,9 @@
+// Учебная программа 
+#include <iostream> 
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+    cout << "Сумма: " << a + b << endl;
